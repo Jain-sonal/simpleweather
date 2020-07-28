b60a0d44fd1bcef1686e366e48ad16fdb4fe1f7b
Sonal Jain <sonaljain.mcs@gmail.com>
2020-07-27 22:00:32 -0500

    Updates

---
+++ precache-manifest.27c43a3d562deca2d47cf046e5185343.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a425a3c4f3f1252f0d97146df03bcd1b",
+    "url": "/simpleweather/index.html"
+  },
+  {
+    "revision": "5d78eaa266c228dea5de",
+    "url": "/simpleweather/static/css/main.81b543a6.chunk.css"
+  },
+  {
+    "revision": "0d237ab3450641f84dca",
+    "url": "/simpleweather/static/js/2.97f8263d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simpleweather/static/js/2.97f8263d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d78eaa266c228dea5de",
+    "url": "/simpleweather/static/js/main.b21d254f.chunk.js"
+  },
+  {
+    "revision": "934c57f4bf0b115e1404",
+    "url": "/simpleweather/static/js/runtime-main.2e1a372b.js"
+  },
+  {
+    "revision": "1ee685edaf7bf4fbb2fe8cbe59a9c739",
+    "url": "/simpleweather/static/media/Cloudy.1ee685ed.jpg"
+  },
+  {
+    "revision": "7f07ceee6411d63675a17d3022238079",
+    "url": "/simpleweather/static/media/Rain.7f07ceee.jpg"
+  },
+  {
+    "revision": "1780e83cef7b4f7ef7d10f58789efc6a",
+    "url": "/simpleweather/static/media/Snow.1780e83c.jpg"
+  },
+  {
+    "revision": "9e71960d6fbdf69943ea9a853591d79f",
+    "url": "/simpleweather/static/media/Sunny.9e71960d.jpg"
+  },
+  {
+    "revision": "ce8c1c91b9e71779aa19cd1fd8519fb8",
+    "url": "/simpleweather/static/media/Thunder.ce8c1c91.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ad5e78d4f6673b3ab4b520968b517eb.js"
+  "/simpleweather/precache-manifest.27c43a3d562deca2d47cf046e5185343.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simpleweather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
